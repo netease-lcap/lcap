4b6f75f740f0356c89dce5b902c3f62ccd40d95e
刘宇阳 <myronliu347@gmail.com>
2024-04-10 16:57:38 +0800

    feat: remove builder change template

--- templates/react/project/vite.config.js
+++ templates/react/project/vite.config.js
@@ -1,27 +1,22 @@
 const { defineConfig } = require('vite');
 const react = require('@vitejs/plugin-react');
+const { lcapPlugin } = require('@lcap/builder');
 const path = require('path');
 
-module.exports = defineConfig(async (configEnv) => {
+module.exports = defineConfig((configEnv) => {
   const pkgInfo = require(`${process.cwd()}/package.json`);
   const defaultConfig = {
-    plugins: [react({ jsxRuntime: 'classic' })],
+    plugins: [
+      react({ jsxRuntime: 'classic' }),
+      lcapPlugin({
+        type: 'extension',
+      }),
+    ],
     build: {
-      target: ['es2020', 'edge88', 'firefox78', 'chrome56', 'safari14'],
       lib: {
         entry: 'src/index.ts',
         name: pkgInfo.name,
         formats: ['es', 'cjs', 'umd'],
-        fileName: (format, entryName) => {
-          switch (format) {
-            case 'es':
-              return `${entryName}.mjs`;
-            case 'cjs':
-              return `${entryName}.cjs`;
-            default:
-              return `${entryName}.js`;
-          }
-        },
       },
       outDir: 'dist-theme',
       sourcemap: false,
@@ -32,13 +27,6 @@ module.exports = defineConfig(async (configEnv) => {
           globals: {
             react: 'React',
             'react-dom': 'ReactDOM',
-          },
-          assetFileNames: (assetInfo) => {
-            if (assetInfo.name === 'style.css') {
-              return 'index.css';
-            }
-
-            return '[name][extname]';
           },
         },
       },
@@ -51,8 +39,8 @@ module.exports = defineConfig(async (configEnv) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '../src'),
-        '@components': path.resolve(__dirname, '../src/components'),
+        '@': path.resolve(__dirname, './src'),
+        '@components': path.resolve(__dirname, './src/components'),
       },
     },
     test: {
