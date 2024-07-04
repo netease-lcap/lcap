3cc2457496c7a44895156b7213ab4278e30d6b49
刘宇阳 <myronliu347@gmail.com>
2024-07-04 17:45:22 +0800

    chore: remove no use modules

--- test/cases/init.test.js
+++
@@ -1,9 +0,0 @@
-const { expect } = require('chai');
-const execa = require('execa');
-
-describe('lcap', () => {
-    it('init', async () => {
-        const { stdout } = await execa.sync('node', ['./bin/lcap', 'init', 'component', 'custom-button']);
-        expect(stdout.includes('Please input a title')).to.be.true;
-    });
-});
